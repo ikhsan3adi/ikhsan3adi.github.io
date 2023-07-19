cdbd8162e9cb9920e79fb0eea9006082a25df241
Ikhsan Satriadi <ikhsansatriadi175@gmail.com>
2023-07-19 11:15:37 +0700

    Init sveltekit, tailwind, postcss & autoprefixer

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import "../app.css";
+    import type { LayoutData } from './$types';
+    
+    export let data: LayoutData;
+</script>
+
+<slot/>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<h1 class="text-4xl mb-2">Welcome to SvelteKit</h1>
+<p>
+	Visit <a class="text-blue-500 underline" href="https://kit.svelte.dev">kit.svelte.dev</a> to read the
+	documentation
+</p>
+
+<style lang="postcss">
+	:global(html) {
+		font-family: theme(fontFamily.mono);
+	}
+</style>
