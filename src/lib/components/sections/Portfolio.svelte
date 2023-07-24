e1ce7ff0ce4641bf58f46a115708fc713afbb1af
Ikhsan Satriadi <ikhsansatriadi175@gmail.com>
2023-07-24 16:12:01 +0700

    Add smooth scrolling

---
+++ src/lib/components/sections/Portfolio.svelte
@@ -0,0 +1,72 @@
+<script>
+	import Wrappper from '$lib/components/Wrappper.svelte';
+
+	const projects = [
+		{
+			name: 'Auction app',
+			description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi, sed!',
+			tag: ['flutter', 'svelte']
+		},
+		{
+			name: 'Polling app',
+			description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi, sed!',
+			tag: ['flutter', 'svelte']
+		},
+		{
+			name: 'Music app',
+			description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi, sed!',
+			tag: ['flutter', 'svelte']
+		},
+		{
+			name: 'Todo app',
+			description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi, sed!',
+			tag: ['flutter', 'svelte']
+		},
+		{
+			name: 'Todo app',
+			description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi, sed!',
+			tag: ['flutter', 'svelte']
+		}
+	];
+</script>
+
+<section>
+	<Wrappper>
+		<div class="w-full pb-36">
+			<h1
+				class="text-center mx-auto my-10 md:my-12 lg:my-16 dark:text-white duration-500"
+				id="portfolio"
+			>
+				Projects
+			</h1>
+			<div class="w-full flex flex-wrap justify-center gap-4">
+				{#each projects as project}
+					<div class="w-full lg:w-[48.88%] bg-text dark:bg-white">
+						<div
+							class="border-4 border-text dark:border-white bg-custom-1 dark:bg-slate-700 aspect-[3/4] sm:max-md:aspect-[4/3] md:max-lg:aspect-[3/2] xl:aspect-[4/3] -translate-x-1 -translate-y-1 active:translate-x-0 active:translate-y-0 hover:-translate-x-1.5 hover:-translate-y-1.5 duration-200 relative group cursor-pointer"
+						>
+							<div class="h-3/5 md:max-lg:h-3/5 border-b-4 border-text dark:border-white relative">
+								<div
+									class="opacity-0 group-hover:opacity-70 absolute top-0 right-0 m-2 px-4 py-2 rounded-full bg-text duration-200"
+								>
+									<h4 class="text-white">View detail</h4>
+								</div>
+							</div>
+							<div class="py-2 px-4">
+								<h3 class="mb-2 dark:text-white">{project.name}</h3>
+								<p class="dark:text-slate-300">{project.description}</p>
+							</div>
+							<div
+								class="border-t-4 border-text dark:border-white w-full h-12 absolute bottom-0 flex justify-start items-center gap-2 px-2"
+							>
+								{#each project.tag as tag}
+									<p class="bg-custom-2 px-4 py-1 h-max">#{tag}</p>
+								{/each}
+							</div>
+						</div>
+					</div>
+				{/each}
+			</div>
+		</div>
+	</Wrappper>
+</section>
---
+++ src/lib/components/sections/Tools.svelte
@@ -0,0 +1,69 @@
+<script>
+	import Wrappper from '$lib/components/Wrappper.svelte';
+	import waveBg from '$lib/components/graphics/wave-bg.svg';
+</script>
+
+<section
+	class="bg-custom-4 dark:bg-teal-800 border-y-4 border-text dark:border-white bg-no-repeat bg-cover bg-center overflow-hidden relative"
+>
+	<div
+		class="absolute top-0 bottom-0 left-0 right-0 bg-cover bg-center bg-no-repeat scale-[1.05]"
+		style="background-image: url({waveBg});"
+	/>
+	<Wrappper>
+		<div class="w-full pb-36 z-10">
+			<h1
+				class="text-center mx-auto my-10 md:my-12 lg:my-16 dark:text-white duration-500"
+				id="portfolio"
+			>
+				Development tools
+			</h1>
+			<div
+				class="bg-text border-2 border-text w-full h-max flex flex-col md:flex-row justify-stretch"
+			>
+				<div class="flex w-full md:w-[40%] flex-col">
+					<div class="w-full h-[250px] md:h-[350px] bg-accent border-2 border-text">
+						<div class="border-b-4 w-full border-text h-12 flex justify-start">
+							<h4 class="my-auto mx-4">Mobile apps</h4>
+						</div>
+					</div>
+					<div
+						class="w-full h-[200px] md:h-[300px] bg-custom-1 border-2 border-text flex justify-center"
+					>
+						<div class="border-t-4 w-full border-text h-12 self-end flex justify-start">
+							<h4 class="my-auto mx-4">Back-end</h4>
+						</div>
+					</div>
+				</div>
+				<div class="flex w-full md:w-[60%] h-full flex-col">
+					<div
+						class="w-full h-[200px] md:h-[300px] bg-secondary border-2 border-text flex justify-center"
+					>
+						<div class="border-t-4 w-full border-text h-12 self-end flex justify-start">
+							<h4 class="my-auto mx-4">Front-end web</h4>
+						</div>
+					</div>
+					<div class="flex">
+						<div class="flex w-1/2 flex-col">
+							<div class="w-full h-[150px] bg-custom-4 border-2 border-text flex justify-center">
+								<div class="border-t-4 w-full border-text h-12 self-end flex justify-start">
+									<h4 class="my-auto mx-4">DevOps</h4>
+								</div>
+							</div>
+							<div class="w-full h-[200px] bg-custom-3 border-2 border-text">
+								<div class="border-b-4 w-full border-text h-12 flex justify-start">
+									<h4 class="my-auto mx-4">Design</h4>
+								</div>
+							</div>
+						</div>
+						<div class="w-1/2 h-[350px] bg-custom-2 border-2 border-text">
+							<div class="border-b-4 w-full border-text h-12 flex justify-start">
+								<h4 class="my-auto mx-4">Others</h4>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</Wrappper>
+</section>
