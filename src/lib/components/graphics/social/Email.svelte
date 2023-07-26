e37608f902f5b64c29e6929ce93c023a670f8d3b
Ikhsan Satriadi <ikhsansatriadi175@gmail.com>
2023-07-26 14:14:38 +0700

    New icons & svgs added

---
+++ src/lib/components/graphics/social/Email.svelte
@@ -0,0 +1,23 @@
+<!-- Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools -->
+<svg
+	class="fill-current"
+	viewBox="0 -2.5 20 20"
+	version="1.1"
+	xmlns="http://www.w3.org/2000/svg"
+	xmlns:xlink="http://www.w3.org/1999/xlink"
+	fill="#000000"
+>
+	<title>email [#1572]</title>
+	<desc>Created with Sketch.</desc>
+	<defs />
+	<g id="Page-1" stroke="none" stroke-width="1">
+		<g id="Dribbble-Light-Preview" transform="translate(-340.000000, -922.000000)">
+			<g id="icons" transform="translate(56.000000, 160.000000)">
+				<path
+					d="M294,774.474 L284,765.649 L284,777 L304,777 L304,765.649 L294,774.474 Z M294.001,771.812 L284,762.981 L284,762 L304,762 L304,762.981 L294.001,771.812 Z"
+					id="email-[#1572]"
+				/>
+			</g>
+		</g>
+	</g>
+</svg>
---
+++ src/lib/components/graphics/tools/Kotlin.svelte
@@ -0,0 +1,7 @@
+<!-- Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools -->
+<svg fill="#000000" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+	<title>Kotlin</title>
+	<path
+		d="M1.734 32l15.068-15.333 15.198 15.333zM0 0h16l-16 16.667zM17.865 0l-17.865 18.667v13.333l32-32z"
+	/>
+</svg>
