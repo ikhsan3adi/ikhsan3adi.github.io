c0ba73f520cbe1658f026ddfa056c484fd4bf3ab
Ikhsan Satriadi <ikhsansatriadi175@gmail.com>
2023-07-26 14:02:32 +0700

    Hero section improved
    * add typewriter effect
    * fix here & there
    
    Update Emblem.svelte

--- src/lib/components/sections/Hero.svelte
+++ src/lib/components/sections/Hero.svelte
@@ -1,54 +1,62 @@
-<script>
+<script lang="ts">
+	import type { ColorVariant } from '$lib/components/buttons/button';
+
+	import Typewriter from 'svelte-typewriter/Typewriter.svelte';
+
 	import bg from '$lib/components/graphics/hero-bg.svg';
 	import Emblem from '$lib/components/graphics/Emblem.svelte';
 	import Wrappper from '$lib/components/Wrappper.svelte';
+	import Button from '$lib/components/buttons/Button.svelte';
+
+	const secondaryButton: ColorVariant = { key: 'secondary' };
 </script>
 
 <section>
 	<div
-		class="h-[1000px] md:h-[870px] bg-cover bg-no-repeat flex"
+		class="h-[950px] md:h-[800px] bg-cover bg-no-repeat bg-center flex relative duration-500"
 		style="background-image: url({bg});"
 	>
-		<Wrappper>
-			<div class="w-full md:flex md:gap-4 md:justify-between my-auto">
-				<div class="mb-52 mt-16 md:m-0 py-2 md:py-8 lg:py-9 sm:max-md:text-center">
-					<h4 class="dark:text-white">Hello, my name is</h4>
-					<h1 class="mb-2 dark:text-white">
-						Ikhsan <h1 class="inline text-primary">Satriadi</h1>
-					</h1>
-					<h4 class="mb-4 dark:text-white">A mobile developer</h4>
+		<div class="md:mt-16 w-full flex">
+			<Wrappper>
+				<div class="w-full md:flex md:gap-4 md:justify-between my-auto">
+					<div class="mb-52 mt-16 md:m-0 py-2 md:py-8 lg:py-9 sm:max-md:text-center duration-500">
+						<h4 id="home" class="dark:text-white duration-500">Hello, my name is</h4>
+						<h1 class="mb-2 dark:text-white duration-500">
+							Ikhsan <h1 class="inline text-primary">Satriadi</h1>
+						</h1>
 
-					<div class="flex gap-3 sm:max-md:justify-center">
-						<button class="bg-text dark:bg-primary">
-							<div
-								class="bg-primary px-4 py-2 -translate-x-1 -translate-y-1 border-text dark:border-primary border-2 dark:bg-text"
-							>
-								<h4 class="dark:text-primary">See projects</h4>
-							</div>
-						</button>
+						<h4 class="mb-4 dark:text-white duration-500">
+							<Typewriter mode={'loop'} interval={25}>
+								A
+								<span class="text-emerald-600 dark:text-primary">mobile developer</span>
+								<span class="text-green-600 dark:text-green-400">back-end developer</span>
+								<span class="text-orange-600 dark:text-orange-400">front-end developer</span>
+							</Typewriter>
+						</h4>
 
-						<button class="bg-text dark:bg-white">
-							<div
-								class="bg-background px-4 py-2 -translate-x-1 -translate-y-1 border-text dark:border-white border-2 dark:bg-text"
-							>
-								<h4 class="dark:text-white">Contact me</h4>
-							</div>
-						</button>
+						<div class="flex gap-3 sm:max-md:justify-center">
+							<a href="#portfolio">
+								<Button noDarkVariant={false}>See projects</Button>
+							</a>
+							<a href="#contact">
+								<Button variant={secondaryButton} noDarkVariant={false}>Contact me</Button>
+							</a>
+						</div>
+					</div>
+					<div class="max-md:mx-auto w-max md:my-auto relative">
+						<div
+							class="w-52 h-52 mx-auto md:w-64 md:h-64 lg:w-72 lg:h-72 dark:text-white absolute -top-3 right-2 duration-[50ms]"
+						>
+							<Emblem />
+						</div>
+						<div
+							class="w-52 h-52 mx-auto md:w-64 md:h-64 lg:w-72 lg:h-72 text-secondary dark:text-transparent dark:stroke-[4] dark:stroke-custom-3 duration-500"
+						>
+							<Emblem />
+						</div>
 					</div>
 				</div>
-				<div class="max-md:mx-auto w-max md:my-auto relative">
-					<div
-						class="w-52 h-52 mx-auto md:w-64 md:h-64 lg:w-72 lg:h-72 dark:text-white absolute -top-3 right-2"
-					>
-						<Emblem />
-					</div>
-					<div
-						class="w-52 h-52 mx-auto md:w-64 md:h-64 lg:w-72 lg:h-72 text-secondary dark:text-transparent dark:stroke-[4] dark:stroke-custom-3"
-					>
-						<Emblem />
-					</div>
-				</div>
-			</div>
-		</Wrappper>
+			</Wrappper>
+		</div>
 	</div>
 </section>
