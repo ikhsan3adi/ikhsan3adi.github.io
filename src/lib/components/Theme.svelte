5ca55ea237d33dac627e96808a2e6a17e159b6d7
Ikhsan Satriadi <ikhsansatriadi175@gmail.com>
2023-07-26 14:10:13 +0700

    Navbar, Hamburger & Theme improved
    * store theme & hamburger menu state using writable store
    * autohide & color changing navbar
    * autohide hamburger on scroll

--- src/lib/components/Theme.svelte
+++ src/lib/components/Theme.svelte
@@ -1,32 +1,20 @@
 <script lang="ts">
+	import { darkEnabled } from '$lib/config';
 	import { onMount } from 'svelte';
 	import Moon from '$lib/components/graphics/Moon.svelte';
 
-	// indicate if we're in dark mode or not
-	let dark: boolean;
+	export let isBgTransparent = true;
 
-	// hide the control until we've decided what the intial mode is
-	let hidden = true;
+	// indicate if we're in dark mode or not
+	let dark = false;
 
 	onMount(() => {
-		// use the existence of the dark class on the html element for the initial value
-		dark = document.documentElement.classList.contains('dark');
+		darkEnabled.set(localStorage.theme == 'dark');
 
-		// show UI controls
-		hidden = false;
-
-		// listen for changes so we auto-adjust based on system settings
-		const matcher = window.matchMedia('(prefers-color-scheme: dark)');
-		matcher.addEventListener('change', handleChange);
-		return () => matcher.removeEventListener('change', handleChange);
+		darkEnabled.subscribe((value) => {
+			if (dark != value) setMode(value);
+		});
 	});
-
-	function handleChange({ matches: dark }: MediaQueryListEvent) {
-		// only set if we haven't overridden the theme
-		if (!localStorage.theme) {
-			setMode(dark);
-		}
-	}
 
 	function toggle() {
 		setMode(!dark);
@@ -34,6 +22,7 @@
 
 	function setMode(value: boolean) {
 		dark = value;
+		darkEnabled.set(value);
 
 		// update page styling
 		if (dark) {
@@ -44,21 +33,12 @@
 
 		// store the theme as a local override
 		localStorage.theme = dark ? 'dark' : 'light';
-
-		// if the toggled-to theme matches the system defined theme, clear the local override
-		// this effectively provides a way to override or revert to "automatic" setting mode
-		if (window.matchMedia(`(prefers-color-scheme: ${localStorage.theme})`).matches) {
-			localStorage.removeItem('theme');
-		}
 	}
 </script>
 
 <svelte:head>
 	<script>
-		if (
-			localStorage.theme === 'dark' ||
-			(!localStorage.theme && window.matchMedia('(prefers-color-scheme: dark)').matches)
-		) {
+		if (localStorage.theme === 'dark') {
 			document.documentElement.classList.add('dark');
 		} else {
 			document.documentElement.classList.remove('dark');
@@ -67,36 +47,43 @@
 </svelte:head>
 
 <button
-	class="{dark
+	class="{dark && isBgTransparent
 		? 'border-white focus:ring-white'
 		: 'border-text focus:ring-text focus:ring-offset-text'} border-2 relative inline-flex flex-shrink-0 h-6 w-11 items-center px-0.5 rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:ring-1 focus:ring-offset-1"
-	class:hidden
 	on:click={toggle}
 >
 	<span
 		class="{dark
 			? 'translate-x-5'
-			: 'translate-x-0'} pointer-events-none relative inline-block h-4 w-4 rounded-full shadow transform ring-0 transition ease-in-out duration-200"
+			: 'translate-x-0'} translate-y-0 pointer-events-none relative inline-block h-4 w-4 rounded-full shadow transform ring-0 transition ease-in-out duration-200"
 	>
 		<span
 			class="{dark
-				? 'opacity-100 ease-in duration-200'
-				: 'opacity-0 ease-out duration-100'} absolute inset-0 h-full w-full flex items-center justify-center transition-opacity"
+				? 'opacity-100 ease-in-out duration-800'
+				: 'opacity-0 ease-in-out duration-800'} translate-y-0 absolute inset-0 h-full w-full flex items-center justify-center transition-opacity"
 			aria-hidden="true"
 		>
 			<!-- moon icon -->
-			<div class="w-4 h-4 text-black dark:text-white">
+			<div
+				class="w-4 h-4 text-black translate-y-0 {isBgTransparent
+					? 'dark:text-white'
+					: 'dark:text-text'} duration-800"
+			>
 				<Moon />
 			</div>
 		</span>
 		<span
 			class="{dark
-				? 'opacity-0 ease-out duration-100'
-				: 'opacity-100 ease-in duration-200'} absolute inset-0 h-full w-full flex items-center justify-center transition-opacity"
+				? 'opacity-0 ease-in-out duration-800'
+				: 'opacity-100 ease-in-out duration-800'} translate-y-0 absolute inset-0 h-full w-full flex items-center justify-center transition-opacity"
 			aria-hidden="true"
 		>
 			<!-- sun icon -->
-			<div class="h-4 w-4 bg-text rounded-full dark:bg-white" />
+			<div
+				class="h-4 w-4 rounded-full translate-y-0 {isBgTransparent
+					? 'dark:bg-white'
+					: 'dark:bg-text'} bg-text"
+			/>
 		</span>
 	</span>
 </button>
--- src/lib/components/sections/Navbar.svelte
+++ src/lib/components/sections/Navbar.svelte
@@ -1,24 +1,109 @@
-<script>
+<script lang="ts">
 	import Wrappper from '$lib/components/Wrappper.svelte';
 	import Theme from '$lib/components/Theme.svelte';
+	import { onMount } from 'svelte';
+	import Hamburger from '$lib/components/buttons/Hamburger.svelte';
+	import { hamburgerMenuExpanded } from '$lib/config';
+	import { navLinks } from '$lib/variables';
+
+	let navbar: HTMLElement;
+
+	let scrollY = 0;
+	let prevScrollpos = scrollY;
+	let prevScrollpos2 = scrollY;
+
+	let isBgTransparent = true;
+	let backgroundClasses = 'bg-opacity-0 border-opacity-0';
+
+	onMount(() => {
+		changeColors();
+		window.onscroll = async () => {
+			autoHideMenu();
+			autoHideNavbar();
+			changeColors();
+		};
+	});
+
+	function autoHideNavbar() {
+		let currentScrollPos = scrollY;
+		if (prevScrollpos > currentScrollPos) {
+			navbar.style.top = '0';
+		} else {
+			navbar.style.top = '-68px';
+		}
+		prevScrollpos = currentScrollPos;
+	}
+
+	function changeColors() {
+		if (scrollY < 300) {
+			backgroundClasses = 'bg-opacity-0 border-opacity-0';
+		} else if (scrollY < 400) {
+			backgroundClasses = 'bg-opacity-20 border-opacity-0';
+		} else if (scrollY < 600) {
+			backgroundClasses = 'bg-opacity-50 border-opacity-0';
+		} else if (scrollY < 800) {
+			isBgTransparent = true;
+			backgroundClasses = 'bg-opacity-60 border-opacity-0';
+		} else {
+			isBgTransparent = false;
+			backgroundClasses = 'bg-opacity-100 border-opacity-100';
+		}
+	}
+
+	function autoHideMenu() {
+		let currentScrollPos = scrollY;
+		if (currentScrollPos > prevScrollpos2 + 250 || currentScrollPos < prevScrollpos2 - 250) {
+			hamburgerMenuExpanded.set(false);
+			prevScrollpos2 = currentScrollPos;
+		}
+	}
 </script>
 
+<svelte:window bind:scrollY />
+
 <section
-	class="fixed top-0 left-0 right-0 bg-primary bg-opacity-30 border-b-4 border-text border-opacity-0"
+	bind:this={navbar}
+	class="fixed top-0 left-0 right-0 bg-primary border-b-4 border-text z-40 duration-300 {backgroundClasses}"
 >
 	<Wrappper>
 		<div class="my-auto h-16 flex justify-between w-full">
 			<!-- Logo -->
 			<div class="my-auto">
 				<div class="mt-2 dark:mt-0 relative">
-					<h3 class="text-primary dark:text-white font-mechsuit md:text-2xl z-0">IKHSaN3ADI</h3>
-					<h3 class="font-mechsuit dark:hidden md:text-2xl absolute -top-1 right-1">IKHSaN3ADI</h3>
+					<a href="/">
+						<h3
+							class="text-primary {isBgTransparent
+								? 'dark:text-white'
+								: 'dark:text-text'} font-mechsuit text-sm md:text-lg z-0 transition-colors duration-500"
+						>
+							IKHSaN3ADI
+						</h3>
+
+						<h3
+							class="font-mechsuit dark:hidden text-sm md:text-lg absolute -top-1 right-1 transition-colors duration-500"
+						>
+							IKHSaN3ADI
+						</h3>
+					</a>
 				</div>
 			</div>
 
+			<!-- Nav links -->
+			<div
+				class="hidden w-full pr-12 xl:pr-16 pl-16 xl:pl-24 lg:flex justify-evenly flex-wrap items-center text-text
+				{isBgTransparent ? 'dark:text-white' : 'dark:text-text'}"
+			>
+				{#each navLinks as link}
+					<a class="hover:underline active:text-indigo-500" href="#{link.link}">{link.text}</a>
+				{/each}
+			</div>
+
+			<!-- Hamburger menu -->
+			<Hamburger {isBgTransparent} />
+
 			<!-- Dark mode toggle -->
-			<div class="my-auto">
-				<Theme />
+			<div class="hidden lg:block my-auto">
+				<Theme {isBgTransparent} />
 			</div>
 		</div>
 	</Wrappper>
