aa0016d40fc5ae44903773b57649e21a2e3e4ed6
Ikhsan Satriadi <ikhsansatriadi175@gmail.com>
2023-08-04 18:37:15 +0700

    Update Wrappper.svelte
    * reduce padding in smaller screen

--- src/lib/components/widgets/Wrappper.svelte
+++ src/lib/components/widgets/Wrappper.svelte
@@ -1,3 +1,3 @@
-<div class="container mx-auto px-16 sm:px-20 md:px-24 lg:px-36 w-full flex">
+<div class="container mx-auto px-10 sm:px-16 md:px-20 lg:px-28 xl:px-32 w-full flex">
 	<slot />
 </div>
