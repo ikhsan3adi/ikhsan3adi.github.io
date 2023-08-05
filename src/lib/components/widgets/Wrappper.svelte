9a7be645f825255c0e4eb048fcb2b2eb121a5a15
Ikhsan Satriadi <ikhsansatriadi175@gmail.com>
2023-08-05 10:42:30 +0700

    Update Wrappper.svelte
    * reduce padding

--- src/lib/components/widgets/Wrappper.svelte
+++ src/lib/components/widgets/Wrappper.svelte
@@ -1,3 +1,3 @@
-<div class="container mx-auto px-10 sm:px-16 md:px-20 lg:px-28 xl:px-32 w-full flex">
+<div class="container mx-auto px-10 sm:px-16 md:px-20 lg:px-24 xl:px-28 w-full flex">
 	<slot />
 </div>
