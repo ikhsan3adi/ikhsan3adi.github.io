ea9675796f157e68a60b0a195e5f82452f2b06c8
ikhsan3adi <ikhsan3adi@users.noreply.github.com>
2024-07-16 04:49:34 +0000

    deploy: 9929ed528a70a4c5a3c748cd91124b38222bfa5b

---
+++ _app/immutable/chunks/loop.05a65f8e.js
@@ -0,0 +1,1 @@
+import{w as i}from"./writeAndUnwriteText.65957df6.js";import{a as n,m}from"./animationSetup.6677d923.js";const p=async(t,e)=>{const{options:o,elements:a}=n(t,e);for(;;){m(t);for(const s of a)await i(s,o)}};export{p as default};
