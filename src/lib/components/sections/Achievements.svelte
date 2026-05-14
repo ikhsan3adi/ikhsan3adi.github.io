2a4884261d6b5e6ba1ca26998a4dd4ab0c9bed39
Ikhsan Satriadi <ikhsansatriadi175@gmail.com>
2026-05-14 23:17:16 +0700

    Update Achievements.svelte
    fix wrong heading id

--- src/lib/components/sections/Achievements.svelte
+++ src/lib/components/sections/Achievements.svelte
@@ -29,7 +29,7 @@
         <Saos animation={'scale-up-center 1s cubic-bezier(0.4, 0, 0.2, 1) both'} once>
           <h1
             class="text-center mx-auto my-10 md:my-12 lg:my-16 dark:text-white duration-500"
-            id="tools"
+            id="achievements"
           >
             Achievements
           </h1>
